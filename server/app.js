1a7fe682c2616587e67151fef98f44f6498c12e5
Axelbresa <bresanovichaxel43@gmail.com>
2023-06-21 15:38:47 -0300

    en la archivo  app.js se agrego los middlewars

--- server/app.js
+++ server/app.js
@@ -6,8 +6,14 @@ const path = require('path');
 
 const app = express();
 
+
 // Middlewares
 // TODO: Implementar middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,4 +23,7 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+const PUERTO=process.env.PORT || 45635;
+app.listen(45635, () => {
+    console.log('Server on port xxxx')}
+    );
